567bbc71bad77d7d62abc6eb3ab0760f94cc71fe
puccinic <puccinic@uninorte.edu.co>
2020-05-15 13:22:03 -0500

    Added behavior to buttons

--- components/boton.jsx
+++ components/boton.jsx
@@ -1,10 +1,10 @@
-function Buttonsc(props){
-    return <button className = {((props.orientacion == "false") ? "btn-nodo-vertical " : "btn-nodo-horizontal ") + props.nodo }>
+function Buttonsc(props) {
+    return <button className={((props.orientacion == "false") ? "btn-nodo-vertical " : "btn-nodo-horizontal ") + props.nodo}
+    onClick={props.action}>
         {props.text}
     </button>
 }
 
 export default Buttonsc
 
-  
-        
+
--- pages/Control.jsx
+++ pages/Control.jsx
@@ -12,6 +12,13 @@ import Buttonsc from '../components/boton'
 
 function ControlPage() {
     const [modalVisible, setModalVisible] = useState(false)
+    const [sNode, setSNode] = useState(null)
+    
+    function action(id){
+        setSNode(id)
+        setModalVisible(true)
+    }
+
     return <Container>
         <Navbarmain dir="Configuracion" text="Configuración de nodos">
             <FaCogs className="iconoconf"/>
@@ -21,18 +28,18 @@ function ControlPage() {
             <img src="/UniGRID_Diagram.png" alt="UniGRID-Diagram" className="imagenprincipal" />
 
             {/* Principio componente */}
-            <Buttonsc orientacion="false" text="C" nodo="Nodo611"/>
-            <Buttonsc orientacion="false" text="A" nodo="Nodo652"/>
-            <Buttonsc orientacion="false" text="A C" nodo="Nodo684"/>
-            <Buttonsc orientacion="false" text="B C" nodo="Nodo645"/>
-            <Buttonsc orientacion="false" text="B C" nodo="Nodo646"/>
-            <Buttonsc orientacion="false" text="A B C" nodo="Nodo632"/>
-            <Buttonsc orientacion="false" text="A B C" nodo="Nodo633"/>
-            <Buttonsc orientacion="false" text="A B C" nodo="Nodo634"/>
-            <Buttonsc orientacion="false" text="A B C" nodo="Nodo675"/>
-            <Buttonsc orientacion="false" text="A B C" nodo="Nodo692"/>
-            <Buttonsc orientacion="true" text="A B C" nodo="Nodo671"/>
-            <Buttonsc orientacion="true" text="A B C" nodo="Nodo680"/>
+            <Buttonsc orientacion="false" text="C" nodo="Nodo611" action={()=> action(611)}/>
+            <Buttonsc orientacion="false" text="A" nodo="Nodo652" action={()=> action(652)}/>
+            <Buttonsc orientacion="false" text="A C" nodo="Nodo684" action={()=> action(684)}/>
+            <Buttonsc orientacion="false" text="B C" nodo="Nodo645" action={()=> action(645)}/>
+            <Buttonsc orientacion="false" text="B C" nodo="Nodo646" action={()=> action(646)}/>
+            <Buttonsc orientacion="false" text="A B C" nodo="Nodo632" action={()=> action(632)}/>
+            <Buttonsc orientacion="false" text="A B C" nodo="Nodo633" action={()=> action(633)}/>
+            <Buttonsc orientacion="false" text="A B C" nodo="Nodo634" action={()=> action(634)}/>
+            <Buttonsc orientacion="false" text="A B C" nodo="Nodo675" action={()=> action(675)}/>
+            <Buttonsc orientacion="false" text="A B C" nodo="Nodo692" action={()=> action(692)}/>
+            <Buttonsc orientacion="true" text="A B C" nodo="Nodo671" action={()=> action(671)}/>
+            <Buttonsc orientacion="true" text="A B C" nodo="Nodo680" action={()=> action(680)}/>
             {/* Final componente */}
 
 
@@ -41,7 +48,7 @@ function ControlPage() {
         <Modal show={modalVisible} onHide={() => setModalVisible(false)} size="lg"
             aria-labelledby="contained-modal-title-vcenter"
             centered>
-        <NodeModal id={632} />
+        <NodeModal id={sNode} />
         </Modal>
         </div>
 </Container>
